c91553814673d198cc803651ca3851208d059477
Kate Mashko <katika.life@gmail.com>
2024-01-23 16:45:13 +0300

    feat:add solutions

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -194,8 +194,15 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let reverseStr = '';
+  for (let i = 0; i < str.length; i += 1) {
+    reverseStr = `${str[i]}${reverseStr}`;
+  }
+  if (reverseStr === str) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -231,8 +238,18 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let result;
+  const str = String(num);
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === String(digit)) {
+      result = true;
+      break;
+    } else {
+      result = false;
+    }
+  }
+  return result;
 }
 
 /**
