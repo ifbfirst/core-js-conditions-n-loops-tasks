e738c3371cd3c0d6ff6b4b88989baec910cff7d5
Kate Mashko <katika.life@gmail.com>
2024-02-01 20:51:03 +0300

    add solutions

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -475,8 +475,18 @@ function sortByAsc(/* arr */) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let newStr = str;
+  let str1 = str;
+  for (let i = 0; i < iterations; i += 1) {
+    str1 = newStr;
+    for (let y = 0; y < str1.length; y += 1) {
+      if (y % 2 !== 0) {
+        newStr = `${newStr.replace(`${str1[y]}`, '')}${str1[y]}`;
+      }
+    }
+  }
+  return newStr;
 }
 
 /**
